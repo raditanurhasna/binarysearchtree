d3cc615d3fd5c4677bfc286ed04f5a16df90c67b
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-30 14:17:24 +0700

    Mmembuat constructor 'Node' yang digunakan untuk mengatur nilai-nilai awal dari object

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node
+{
+public:
+	string info;
+	Node* leftchild;
+	Node* ringchild;
+
+	// Constructor for the the node class
+	Node(string i, Node* I, Node* r)
+	{
+
+	}
+
+}
