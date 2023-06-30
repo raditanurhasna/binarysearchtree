19bea313656e94c44809b57c3f10ff085abe1129
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-30 14:46:28 +0700

    Digunakan untuk mencari pencarian biner

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -45,7 +45,18 @@ public:
 			ROOT = newNode; //Mark the new node as ROOT
 			return; //EXIT
 		}
+
+		if (element < parent->info) // If the vqalue in the data field of the new node is less than that of the parent 
+		{
+			parent->leftchild = newNode; // Make the left child of the parent point to the new node
+		}
+		else if (element > parent->info) // If the value in the data field of the new node is greater than that of the parent
+		{
+			parent->rightchild = newNode;
+		}
 	}
+
+
 };
 
 
