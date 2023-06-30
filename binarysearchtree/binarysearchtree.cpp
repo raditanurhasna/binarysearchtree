b1ef43d145b84d538a3b100efbf16e31546f24fb
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-30 14:32:41 +0700

    insert(string element) digunakan untuk memasukkan (menyisipkan) sebuah simpul (node) ke dalam pohon pencarian biner (binary search tree).

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -29,5 +29,16 @@ public:
 		ROOT = NULL; //Initializing ROOT to null
 	}
 
+	void insert(string element) // Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node
+		newNode->info = element; // Assign value to the data filed of the new node 
+		newNode->leftchild = NULL; // Make the left child of the new  node 
+		newNode->rightchild = NULL; // Make the right child of the new node
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+	}
 };
 
